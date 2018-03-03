601be72b3ae7f3c9794dda092ad67e94026d88c5
galiarmero <galileo.armero@gmail.com>
2018-03-03 22:29:28 +0800

    Move prompt for username and password at the start

--- playoff_games_counter.py
+++ playoff_games_counter.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import getpass
 
 from schedule_grid import ScheduleGrid
 from yahoo_nba_fantasy import YahooNbaFantasy
@@ -8,13 +9,15 @@ class PlayoffGamesCounter(object):
     def __init__(self):
         self._driver = webdriver.Chrome(chrome_options=headless_chrome_options)
         self._sched = ScheduleGrid(self._driver)
-        self._yahoo = YahooNbaFantasy(self._driver)
+        self._yahoo_nba_fantasy = YahooNbaFantasy(self._driver)
         self._team_games_per_week = {}
     
-    def main(self):
+    def main(self, username, password):
         self._team_games_per_week = self._sched.get_team_games_per_week([22, 23, 24])
-        self._yahoo.go_to_league(10156)
+        self._yahoo_nba_fantasy.go_to_league(username, password, 10156)
 
 if __name__ == "__main__":
+    username = input("Enter Yahoo username/email: ")
+    password = getpass.getpass(prompt="Enter Yahoo password: ")
     games_counter = PlayoffGamesCounter()
-    games_counter.main()
+    games_counter.main(username, password)
--- yahoo_nba_fantasy.py
+++ yahoo_nba_fantasy.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as expect
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-import getpass
 
 from yahoo_auth import YahooAuth
 
@@ -13,12 +12,10 @@ class YahooNbaFantasy(object):
         self._driver = driver
         self._yahoo_auth = YahooAuth(self._driver)
 
-    def go_to_league(self, league_id):
-        self._login(YAHOO_FANTASY_URL + "/nba/" + str(league_id))
+    def go_to_league(self, username, password, league_id):
+        self._login(username, password, YAHOO_FANTASY_URL + "/nba/" + str(league_id))
 
-    def _login(self, redirect_url):
-        username = input("Enter Yahoo email: ")
-        password = getpass.getpass(prompt="Enter Yahoo password: ")
+    def _login(self, username, password, redirect_url):
         self._yahoo_auth.login(username, password, redirect_url)
             
 
