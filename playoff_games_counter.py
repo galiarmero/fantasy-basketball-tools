09ce05cf1765a65f73088ebfbd987bba0c77a3e7
galiarmero <galileo.armero@gmail.com>
2018-03-03 23:13:09 +0800

    Decouple YahooAuth usage from YahooNbaFantasy

--- playoff_games_counter.py
+++ playoff_games_counter.py
@@ -2,22 +2,28 @@ from selenium import webdriver
 import getpass
 
 from schedule_grid import ScheduleGrid
+from yahoo_auth import YahooAuth
 from yahoo_nba_fantasy import YahooNbaFantasy
 from config import headless_chrome_options
+
+
+YAHOO_FANTASY_URL = "https://basketball.fantasysports.yahoo.com"
 
 class PlayoffGamesCounter(object):
     def __init__(self):
         self._driver = webdriver.Chrome(chrome_options=headless_chrome_options)
         self._sched = ScheduleGrid(self._driver)
+        self._yahoo_auth = YahooAuth(self._driver)
         self._yahoo_nba_fantasy = YahooNbaFantasy(self._driver)
         self._team_games_per_week = {}
     
-    def main(self, username, password):
-        self._team_games_per_week = self._sched.get_team_games_per_week([22, 23, 24])
-        self._yahoo_nba_fantasy.go_to_league(username, password, 10156)
+    def main(self, username, password, league_id, weeks):
+        self._team_games_per_week = self._sched.get_team_games_per_week(weeks)
+        self._yahoo_auth.login(username, password, YAHOO_FANTASY_URL + "/nba/" + str(league_id))
+        self._yahoo_nba_fantasy.get_rosters()
 
 if __name__ == "__main__":
     username = input("Enter Yahoo username/email: ")
     password = getpass.getpass(prompt="Enter Yahoo password: ")
     games_counter = PlayoffGamesCounter()
-    games_counter.main(username, password)
+    games_counter.main(username, password, 10156, [22, 23, 24])
--- yahoo_nba_fantasy.py
+++ yahoo_nba_fantasy.py
@@ -3,20 +3,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as expect
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
-from yahoo_auth import YahooAuth
-
-YAHOO_FANTASY_URL = "https://basketball.fantasysports.yahoo.com"
 
 class YahooNbaFantasy(object):
     def __init__(self, driver):
         self._driver = driver
-        self._yahoo_auth = YahooAuth(self._driver)
 
-    def go_to_league(self, username, password, league_id):
-        self._login(username, password, YAHOO_FANTASY_URL + "/nba/" + str(league_id))
-
-    def _login(self, username, password, redirect_url):
-        self._yahoo_auth.login(username, password, redirect_url)
+    def get_rosters(self):
+        pass
             
 
         
