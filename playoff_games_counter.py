baff78894976abe2b70774dec0161b830db3f0e9
galiarmero <galileo.armero@gmail.com>
2018-03-06 23:23:07 +0800

    Refactor counting of team games per week

--- playoff_games_counter.py
+++ playoff_games_counter.py
@@ -8,8 +8,6 @@ from yahoo_nba_fantasy import YahooNbaFantasy
 from config import headless_chrome_options, YAHOO_FANTASY_URL
 from timer import timer
 
-
-
 class PlayoffGamesCounter(object):
     def __init__(self):
         self._driver = webdriver.Chrome(chrome_options=headless_chrome_options)
@@ -29,35 +27,44 @@ class PlayoffGamesCounter(object):
     def _generate_games_per_week(self, weekly_gms_per_nba_team, team_rosters):
         weekly_games_per_team = {}
         player_count_per_nba_team = {}
-        for week, games in weekly_gms_per_nba_team.items():
-            weekly_games_per_team[str(week)] = []
+        for week, games_per_nba_team in weekly_gms_per_nba_team.items():
+            weekly_games_per_team[week] = []
             for team in team_rosters:
-                team_games_this_week = 0
+                team_games_this_week = \
+                    self._generate_team_games_for_week(team, player_count_per_nba_team, games_per_nba_team)
 
-                if team["name"] not in player_count_per_nba_team:
-                    player_count_per_nba_team[team["name"]] = {}
-                    for player in team["roster"]:
-                        player_team = player["team"]
-                        try:
-                            team_games_this_week = team_games_this_week + games[player_team]
-
-                            if player_team in player_count_per_nba_team[team["name"]]:
-                                player_count_per_nba_team[team["name"]][player_team] = \
-                                    player_count_per_nba_team[team["name"]][player_team] + 1
-                            else:
-                                player_count_per_nba_team[team["name"]][player_team] = 1
-
-                        except KeyError:
-                            print("\t{} not found".format(player["team"]))
-                else:
-                    for nba_team, player_count in player_count_per_nba_team[team["name"]].items():
-                        team_games_this_week = team_games_this_week + (games[nba_team] * player_count)
-
-                weekly_games_per_team[str(week)].append({ 'id' : team['id'], 'name' : team['name'], \
+                weekly_games_per_team[week].append({ 'id' : team['id'], 'name' : team['name'], \
                                                         'games' : team_games_this_week })
                                                         
         with open('weekly_team_games.json', 'w') as outfile:
             json.dump(weekly_games_per_team, outfile)
+
+    def _generate_team_games_for_week(self, team, player_count_per_nba_team, games_per_nba_team):
+        team_games_this_week = 0
+
+        if team["name"] not in player_count_per_nba_team:
+            team_games_this_week = self._count_team_games_and_update_player_count(player_count_per_nba_team, team, team_games_this_week, games_per_nba_team)
+        else:
+            for nba_team, player_count in player_count_per_nba_team[team["name"]].items():
+                team_games_this_week = team_games_this_week + (games_per_nba_team[nba_team] * player_count)
+        return team_games_this_week
+
+    def _count_team_games_and_update_player_count(self, player_count_per_nba_team, team, team_games_this_week, games_per_nba_team):
+        player_count_per_nba_team[team["name"]] = {}
+        for player in team["roster"]:
+            player_team = player["team"]
+            try:
+                team_games_this_week = team_games_this_week + games_per_nba_team[player_team]
+
+                if player_team in player_count_per_nba_team[team["name"]]:
+                    player_count_per_nba_team[team["name"]][player_team] = \
+                        player_count_per_nba_team[team["name"]][player_team] + 1
+                else:
+                    player_count_per_nba_team[team["name"]][player_team] = 1
+
+            except KeyError:
+                print("\t{} not found".format(player["team"]))
+        return team_games_this_week
 
 
 
